462aa92fa752329854d2158604c7d924b9df9b94
Jose A. Menacho Minchola <josmendev@gmail.com>
2024-11-20 07:18:20 -0500

    Creación de la API REST ful

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
+import { json } from 'stream/consumers';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
@@ -11,24 +13,31 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor(options: Options){
-        const { port, public_path = 'public' } = options;
+        const { port, routes, public_path = 'public' } = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
     
     async start() {
         
         // Middlewares: funciones que se ejecutan en todo momento que pasa por una ruta
-
+        this.app.use(express.json());   // raw
+        this.app.use(express.urlencoded({extended: true})); //x-www-form-urlencoded
 
         // Public folder
         // En una primera instancia carga en una ruta que esta dentro de public
         this.app.use(express.static(this.publicPath));
 
+        // Routes
+        this.app.use(this.routes);
+
         // Cuando se recarga la pagina y no encuentra la ruta del archivo dentro de public
         // Entonces pasa aqui
+        // SPA
         this.app.get('*', (req, res) => {
             const indexPath = path.join(__dirname, `../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
